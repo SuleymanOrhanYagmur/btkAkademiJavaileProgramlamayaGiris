237659a9623714d63563d41f9d0e8dc26d8f2fe4
Suleyman Orhan Yağmur <suleymanorhan111@gmail.com>
2025-07-12 01:34:39 +0300

    11. Encapsulation, Attributes, Getter Setter

---
+++ classesWithAttributes/src/Product.java
@@ -0,0 +1,71 @@
+public class Product {
+    //Attribute /field
+    //private sadece tanımlandığı blogta geçerlidir.
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stockAmount;
+    private String renk;
+    private String kod;
+
+    //getter
+    public int getId() { // ürünün id sini değiştiremez ama bu sayede okuyabilir
+        return id;
+    }
+
+    //setter
+    public void setId(int id) {
+        id = id;//_ ile olanlar field attribute yani
+        //this.id = id;  //this.id demek bu classın id si demek, diğeride productun id si
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockAmount() {
+        return stockAmount;
+    }
+
+    public void setStockAmount(int stockAmount) {
+        this.stockAmount = stockAmount;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    public String getKod() {
+        return this.name.substring(0, 1) + id;
+
+    }
+
+}
+
+
+
